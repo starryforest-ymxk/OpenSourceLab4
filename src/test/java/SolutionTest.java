820c25421b8b9760f196603a4f23df69319d92fc
梚辰 <1911308683@qq.com>
2023-11-28 05:47:00 +0800

    init

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/** 
+* Solution Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>11ÔÂ 27, 2023</pre> 
+* @version 1.0 
+*/
+public class SolutionTest {
+
+    @Test
+    public void testCompareVersion(){
+
+        Solution s = new Solution();
+        Assert.assertEquals(s.compareVersion("1.01","1.001"),0);
+        Assert.assertEquals(s.compareVersion("1.0","1.00"),0);
+        Assert.assertEquals(s.compareVersion("0.1","1.1"),-1);
+    }
+
+
+} 
